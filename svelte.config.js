042dfcc59e6f7698db2dea5c35786662bc00486e
Mirko Nitschke <mnitschke93@gmail.com>
2022-06-22 20:05:13 +0200

    Fix config options

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,12 @@ const config = {
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	},
 
 	preprocess: [
