51b10eb456013d9a2cd3d911678f3c1f4434d0f6
Mirko Nitschke <19408385+moekify@users.noreply.github.com>
2022-12-27 18:02:38 +0100

    Add impressum

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,24 +5,65 @@
 
 <Stage>
 	<Item width="w-96" classes="bg-white" dataX="20" dataY="73">
-		<h2 class="font-regular text-4xl pb-2">This place is still under construction</h2>
+		<h2 class="font-regular text-4xl pb-2">Explanation</h2>
 		<p>Drag over the background to draw, double click to clear</p>
 		<p>Resize and drag these boxes as you like!</p>
 	</Item>
 	<Item width="w-96" classes="bg-white" dataX="60" dataY="73">
 		<h2 class="font-regular text-4xl pb-2">Who is Mirko</h2>
 		<p>Entrepreneur, Software Developer, Product Manager, Human</p>
-		<a href="mailto:mirko+website@nitschke.me" data-sveltekit-preload-data
+		<a class="hover:underline" href="mailto:mirko+website@nitschke.me" data-sveltekit-preload-data
 			><p class="text-lg hover:underline">Reach out to Mirko?!</p></a
 		>
 	</Item>
-	<Item width="w-1/5" classes="bg-white" dataX="45" dataY="7"
-		><h2 class="font-regular text-4xl pb-2">What is Mirko?</h2>
+	<Item width="w-90" classes="bg-white" dataX="45" dataY="7"
+		><h2 class="font-regular text-4xl pb-2">What projects is Mirko involed in?</h2>
 		<ul class="prose">
 			<li>
-				<a href="https://www.love-foundation.org" data-sveltekit-preload-data>Love Foundation</a>
+				<a
+					class="hover:underline"
+					href="https://www.love-foundation.org"
+					data-sveltekit-preload-data>Love Foundation</a
+				>
 			</li>
-			<li><a href="https://music.hugrecords.world" data-sveltekit-preload-data>Hug Records</a></li>
+			<li>
+				<a class="hover:underline" href="https://music.hugrecords.world" data-sveltekit-preload-data
+					>Hug Records</a
+				>
+			</li>
+		</ul>
+	</Item>
+	<Item width="w-90" classes="bg-white" dataX="8" dataY="55"
+		><h2 class="font-regular text-4xl pb-2">Offerings</h2>
+		<div class="flex flex-wrap space-x-2 space-y-1">
+			<span
+				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-black"
+				>Web Development</span
+			>
+			<span
+				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-black"
+				>Product Management</span
+			>
+			<span
+				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-black"
+				>Email Services</span
+			>
+			<span
+				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-black"
+				>Hosting</span
+			>
+			<span
+				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-black"
+				>DevOps</span
+			>
+		</div>
+	</Item>
+	<Item width="w-1/5" classes="bg-white" dataX="50" dataY="30"
+		><h2 class="font-regular text-4xl pb-2">Imprint</h2>
+		<ul class="prose">
+			<li>
+				<a class="hover:underline" href="/impressum" data-sveltekit-preload-data>Impressum</a>
+			</li>
 		</ul>
 	</Item>
 	<Item width="w-1/5" classes="bg-white" dataX="5" dataY="5">
@@ -62,6 +103,11 @@
 				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-yellow-100 text-black"
 			>
 				Cypress
+			</span>
+			<span
+				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-yellow-100 text-black"
+			>
+				Typescript
 			</span>
 			<span
 				class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-yellow-100 text-black"
---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,43 @@
+<section class="h-screen">
+	<div class="flex flex-row px-10 py-2">
+		<a class="text-white text-center text-xl hover:underline" href="/">Back</a>
+		<h1 class="text-white text-center text-xl flex-grow">Impressum</h1>
+	</div>
+	<div class="prose text-center w-3/4 m-auto text-white">
+		<h1>Impressum</h1>
+		<p>Angaben gemäß § 5 TMG</p>
+		<p>Mirko Nitschke Grosse Hamburger Strasse 24 10115 Berlin</p>
+		<p>
+			<strong>Vertreten durch: </strong>
+			Mirko Nitschke
+		</p>
+		<p>
+			<strong>Kontakt:</strong>
+			Telefon: 030-12109747 E-Mail: <a href="mailto:hello@nitschke.me">hello@nitschke.me</a>
+		</p>
+		<p>
+			<strong>Umsatzsteuer-ID: </strong>
+			Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: DE354294315
+		</p>
+		<p>
+			<strong>Haftungsausschluss: </strong><br /><br /><strong>Haftung für Inhalte</strong>
+			Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit
+			und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind
+			wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
+			Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
+			gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+			Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen
+			nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch
+			erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden
+			von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br /><br
+			/><strong>Haftung für Links</strong><br /><br />
+			Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss
+			haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte
+			der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich.
+			Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+			Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
+			Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht
+			zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+		</p>
+	</div>
+</section>
